051027c973aa7df2c7d0efda0b1392280f375442
ww24e018 <ww24e018@technikum-wien.at>
2025-05-19 12:57:53 +0200

    the controll, according to tutorial

---
+++ src/main/java/at/technikum/springrestbackend/controller/KarteiController.java
@@ -0,0 +1,32 @@
+package at.technikum.springrestbackend.controller;
+
+
+import at.technikum.springrestbackend.entity.Kartei;
+import at.technikum.springrestbackend.repository.KarteiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/kartei")
+public class KarteiController {
+
+    @Autowired
+    private KarteiRepository karteiRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewKartei(@RequestParam String name, @RequestParam String beschreibung) {
+
+        Kartei k = new Kartei();
+        k.setName(name);
+        k.setBeschreibung(beschreibung);
+        karteiRepository.save(k);
+        return "saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Kartei> getAllKarteien() {
+        return karteiRepository.findAll();
+    }
+
+}
