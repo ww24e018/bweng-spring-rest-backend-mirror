707c19cec4f0c845a32b4f0d353822cda8e708b9
ww24e018 <ww24e018@technikum-wien.at>
2025-05-19 12:48:17 +0200

    added the "repository"

---
+++ src/main/java/at/technikum/springrestbackend/repository/KarteiRepository.java
@@ -0,0 +1,7 @@
+package at.technikum.springrestbackend.repository;
+
+import at.technikum.springrestbackend.entity.Kartei;
+import org.springframework.data.repository.CrudRepository;
+
+public interface KarteiRepository extends CrudRepository<Kartei, Integer> {
+}
