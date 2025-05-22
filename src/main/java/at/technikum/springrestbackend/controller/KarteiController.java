653bae7ba027b7e9e1f0465df772d4a74b6359c2
ww24e018 <ww24e018@technikum-wien.at>
2025-05-22 09:07:40 +0200

    @CrossOrigin needed for testing

--- src/main/java/at/technikum/springrestbackend/controller/KarteiController.java
+++ src/main/java/at/technikum/springrestbackend/controller/KarteiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @Controller
 @RequestMapping(path = "/kartei")
 public class KarteiController {
